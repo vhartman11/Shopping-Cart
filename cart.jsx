4f694158b0e541a8e648b002dbe4481ab4a2efc3
Vince Hartman <97495456+vhartman11@users.noreply.github.com>
2022-07-04 21:30:43 -0400

    Add files via upload
    added inventory control

--- cart.jsx
+++ cart.jsx
@@ -102,6 +102,8 @@ const Products = (props) => {
   const addToCart = (e) => {
     let name = e.target.name;
     let item = items.filter((item) => item.name == name);
+    if (item[0].instock == 0) return;
+    item[0].instock = item[0].instock - 1;
     console.log(`add to Cart ${JSON.stringify(item)}`);
     setCart([...cart, ...item]);
     //doFetch(query);
